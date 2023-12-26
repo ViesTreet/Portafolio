9ecf19a9ad69a34545df67e6ac474b88f5e30349
Javier Ulloa <javierulloafierro@gmail.com>
2023-12-26 18:03:55 -0300

    Base Lista

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var boton = document.querySelector('.BotonConoceme');
+  var boton = document.querySelector('.BotonConoceme');
 
-    function activarAnimacion() {
-        if (!boton.classList.contains('hovered')) {
-            boton.classList.add('Saltos');
-            setTimeout(function () {
-                boton.classList.remove('Saltos');
-            }, 3000); // 3 segundos para la duración de la animación
-        }
+  function activarAnimacion() {
+    if (!boton.classList.contains('hovered')) {
+      boton.classList.add('Saltos');
+      setTimeout(function () {
+        boton.classList.remove('Saltos');
+      }, 3000); // 3 segundos para la duración de la animación
     }
+  }
 
-    // Inicia la animación cada cierto intervalo
-    setInterval(activarAnimacion, 7000); // 7 segundos en total (3 segundos de animación + 4 segundos de retraso)
-    
-    // Agrega y quita la clase "hovered" durante el hover
-    boton.addEventListener('mouseenter', function () {
-        boton.classList.add('hovered');
+  // Inicia la animación cada cierto intervalo
+  setInterval(activarAnimacion, 7000); // 7 segundos en total 
+
+
+  boton.addEventListener('mouseenter', function () {
+    boton.classList.add('hovered');
+  });
+
+  boton.addEventListener('mouseleave', function () {
+    boton.classList.remove('hovered');
+  });
+
+  const elementoADesaparecer = document.getElementById('presentacionProyecto');
+  let tercero = document.getElementById('tercero');
+
+  // Opciones para IntersectionObserver
+  const opciones = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1 // Define cuánto del elemento debe estar visible para activar la función
+  };
+
+  // Crear una instancia de IntersectionObserver con la función de callback
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        elementoADesaparecer.style.opacity = '1';
+        setTimeout(function () {
+          //elementoADesaparecer.style.display = 'flex';
+        }, 2500); // 3 segundo
+        observer.unobserve(entry.target);
+      }
+    });
+  }, opciones);
+
+  const projects = document.querySelectorAll('.project');
+
+  projects.forEach(project => {
+    project.addEventListener('mouseenter', function () {
+      this.querySelector('.project-img').style.opacity = '0'; // Oculta la imagen
+      this.querySelector('.project-info').style.opacity = '1'; // Muestra el texto informativo
     });
 
-    boton.addEventListener('mouseleave', function () {
-        boton.classList.remove('hovered');
+    project.addEventListener('mouseleave', function () {
+      this.querySelector('.project-img').style.opacity = '1'; // Muestra la imagen
+      this.querySelector('.project-info').style.opacity = '0'; // Oculta el texto informativo
     });
+  });
+
+  // Observar la sección tercera
+  observer.observe(tercero);
 });
